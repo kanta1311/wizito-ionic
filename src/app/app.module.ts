1b08189eb381a0e2ce1cadca3f7229d5f832cc79
Nazim Patel <nazim@techsevin.com>
2018-05-08 12:50:52 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { WelcomePage } from '../pages/welcome/welcome';
 @NgModule({
   declarations: [
     MyApp,
